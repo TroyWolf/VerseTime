3da36d4cebaa5ddd899431209f1574a256761ab8
Troy Wolf <troy@troywolf.com>
2024-11-30 08:31:51 -0600

    Route to fetch an entire chapter as well as random matching chapter and verse.

--- src/bible/routes.js
+++ src/bible/routes.js
@@ -1,12 +1,31 @@
 import bibleSvc from "./service.js"
 
 export default (app) => {
-  app.get("/:book/:chapter/:verse", async (req, res, next) => {
+  app.get("/verse/:book/:chapter/:verse", async (req, res, next) => {
     const { book, chapter, verse } = req.params
-    const scripture = await bibleSvc.getVerse({ book, chapter, verse })
-    if (scripture.code) {
-      res.statusCode(scripture.code)
+    const data = await bibleSvc.getVerse({ book, chapter, verse })
+    if (data.code) {
+      res.statusCode(data.code)
     }
-    res.json(scripture)
+    res.json(data)
   })
+  
+  app.get("/chapter/:book/:chapter", async (req, res, next) => {
+    const { book, chapter, verse } = req.params
+    const data = await bibleSvc.getChapter({ book, chapter, verse })
+    if (data.code) {
+      res.statusCode(data.code)
+    }
+    res.json(data)
+  })
+
+  app.get("/time/:chapter/:verse", async (req, res, next) => {
+    const { book, chapter, verse } = req.params
+    const data = await bibleSvc.getTimeVerse({ book, chapter, verse })
+    if (data.code) {
+      res.statusCode(data.code)
+    }
+    res.json(data)
+  })
+
 }
--- src/bible/service.js
+++ src/bible/service.js
@@ -8,6 +8,24 @@ const getVerse = async ({book, chapter, verse}) => {
   return result.rows[0]
 }
 
+const getChapter = async ({book, chapter, verse}) => {
+  const result = await doQuery('bible.chapter', { book, chapter })
+  if (!result.rows[0]) {
+    return {code: 404}
+  }
+  return result.rows
+}
+
+const getTimeVerse = async ({chapter, verse}) => {
+  const result = await doQuery('bible.timeVerse', { chapter, verse })
+  if (!result.rows[0]) {
+    return {code: 404}
+  }
+  return result.rows[Math.floor(Math.random() * result.rows.length)]
+}
+
 export default {
   getVerse,
+  getChapter,
+  getTimeVerse,
 }
