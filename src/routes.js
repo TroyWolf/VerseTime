6a95be09c7ada8b014c22f0db5d0b30b1ac1675d
Troy Wolf <troy@troywolf.com>
2024-12-02 15:43:42 -0600

    Remove the test route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
-import testRoutes from './test/routes.js'
 import bibleRoutes from './bible/routes.js'
 
 export default (app) => {
-  testRoutes(app)
   bibleRoutes(app)
 }
--- src/test/routes.js
+++
@@ -1,8 +0,0 @@
-export default (app) => {
-  app.get("/test", async (req, res, next) => {
-    const data = {
-      test: "YO! YO! YO! This came from the API, bro!",
-    }
-    res.json(data)
-  })
-}
