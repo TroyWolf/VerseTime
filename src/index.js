92b0865acb7b14a284c17a652c941ea320b5f2eb
Troy Wolf <troy@troywolf.com>
2024-12-03 13:37:29 -0600

    oops

--- src/index.js
+++ src/index.js
@@ -56,10 +56,10 @@ const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 })
-*/
 
 //  apply to all requests
 app.use(limiter)
+*/
 
 app.use(logger('dev'));
 
